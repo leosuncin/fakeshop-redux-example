c5cdef8998990e22d4a61f4c41420ae120ac80cf
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2021-12-03 23:40:08 -0600

    test: add integration test for the app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,16 +1,54 @@
 import { render, screen } from '@testing-library/react';
+import user from '@testing-library/user-event';
 import { expect } from 'chai';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
+import { createStore } from './app/store';
+import products from './fixtures/products.json';
 
 describe('<App>', () => {
-  it('renders learn react link', () => {
-    render(<App />);
+  it('renders the app', () => {
+    const store = createStore();
+
+    render(
+      <Provider store={store}>
+        <Router>
+          <App />
+        </Router>
+      </Provider>,
+    );
 
     expect(
       document.body.contains(
-        screen.getByRole('link', { name: /learn react/i }),
+        screen.getByRole('heading', { name: /fakeshop/i }),
       ),
     );
+  });
+
+  it("navigate to product's details", async () => {
+    const [product] = products;
+    const store = createStore({
+      products: {
+        status: 'idle',
+        products,
+        product,
+      },
+    });
+
+    render(
+      <Provider store={store}>
+        <Router>
+          <App />
+        </Router>
+      </Provider>,
+    );
+
+    user.click(screen.getByText(product.title));
+
+    await screen.findByText(product.title);
+
+    expect(screen.getByText(product.title)).to.exist;
   });
 });
